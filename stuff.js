6cad494ce25f562f7029ed649400dd4c7e90cdbf
Varsha Jacob <varshajacob15@gmail.com>
2023-10-14 23:37:07 -0400

    add submit button

---
+++ stuff.js
@@ -0,0 +1,9 @@
+let loginForm = document.getElementById("loginForm");
+
+console.log("JS linked");
+
+
+function myFunction() {
+    document.getElementById("button").style.color = "red";
+    console.log("Worked");
+  }
